bd8010884956b02e63b00c721bbaa7966b85b44f
Mohammedi Abdenasser <75934365+mdinacer@users.noreply.github.com>
2022-03-15 14:07:47 +0100

    Added Axios service agent

---
+++ src/utils/agent.ts
@@ -0,0 +1,114 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+
+const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+// In case cross-site Access-Control requests should be made using credentials
+//axios.defaults.withCredentials = true;
+
+/**
+ * Configure the request headers with Authorization Header using the authentication token
+ */
+// axios.interceptors.request.use(config => {
+//     const token = GetToken()
+//     if (token && config.headers) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// });
+
+axios.interceptors.response.use(async response => {
+
+    // Add response time delay to simulate external API calls delay.
+    if (process.env.NODE_ENV === "development") {
+        await sleep();
+    }
+
+
+    // Parse the Pagination header from the response 
+    // const pagination = response.headers["pagination"];
+    // if (pagination) {
+    //     HANDLE PAGINATION RESULTS
+    //     return response;
+    // }
+
+    return response;
+
+}, (error: AxiosError) => {  // Handle the errors from server
+    if (error.response) {
+        const { data, status } = error.response;
+
+        switch (status) {
+            case 400:
+                // Handle Clients Errors
+                break;
+
+            case 401:
+                // Handle Authentication Errors
+                break;
+
+            case 403:
+                // Handle Authorization Errors
+                break;
+
+            case 404:
+                // Handle Not Found Errors, Generally navigate to a Not Found page.
+                break;
+
+            case 500:
+                // Handle Server Errors, Generally navigate to a Server Error page.
+                break;
+
+            default:
+                break;
+        }
+    }
+    return Promise.reject(error.response);
+})
+
+
+/**
+ * Create a simple data form.
+ * TODO: add flattening complex objects.
+ */
+function createFormData(item: any) {
+    let formData = new FormData();
+    for (const key in item) {
+        formData.append(key, item[key])
+    }
+    return formData
+}
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+//Axios request simplified
+// the T Class type is options but provides a better type safety for return type
+const requests = {
+    get: <T>(url: string, params?: URLSearchParams) => axios.get<T>(url, { params }).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    patch: <T>(url: string, body: {}) => axios.patch<T>(url, body).then(responseBody),
+    delete: <T>(url: string, body?: {}) => axios.delete<T>(url, body).then(responseBody),
+    postForm: (url: string, data: FormData) => axios.post(url, data, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    }).then(responseBody),
+    putForm: (url: string, data: FormData) => axios.put(url, data, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    }).then(responseBody),
+}
+
+const Projects = {
+    list: () => requests.get('projects'),
+}
+
+const Ideas = {
+}
+
+const agent = {
+    Projects,
+    Ideas
+}
+
+export default agent;
