b672531c8760a0e62c2504754564b7c96a357060
Mohammedi Abdenasser <75934365+mdinacer@users.noreply.github.com>
2022-03-15 14:46:56 +0100

    cleaned the axios service agent

--- src/utils/agent.ts
+++ src/utils/agent.ts
@@ -2,12 +2,9 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { Project } from "../components/common/filtering/project";
 
 
-const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
-
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
-
-// In case cross-site Access-Control requests should be made using credentials
+// In case of cross-site Access-Control requests should be made using credentials
 //axios.defaults.withCredentials = true;
 
 /**
@@ -23,13 +20,13 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 axios.interceptors.response.use(async response => {
 
-    // Add response time delay to simulate external API calls delay.
     if (process.env.NODE_ENV === "development") {
-        await sleep();
+        // execute codes for dev environment
     }
 
 
-    // Parse the Pagination header from the response 
+    // Parses the Pagination header from the response 
+
     // const pagination = response.headers["pagination"];
     // if (pagination) {
     //     HANDLE PAGINATION RESULTS
@@ -38,7 +35,7 @@ axios.interceptors.response.use(async response => {
 
     return response;
 
-}, (error: AxiosError) => {  // Handle the errors from server
+}, (error: AxiosError) => {  // Handle the exceptions sent from server
     if (error.response) {
         const { data, status } = error.response;
 
@@ -62,9 +59,6 @@ axios.interceptors.response.use(async response => {
             case 500:
                 // Handle Server Errors, Generally navigate to a Server Error page.
                 break;
-
-            default:
-                break;
         }
     }
     return Promise.reject(error.response);
@@ -85,8 +79,8 @@ function createFormData(item: any) {
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-//Axios request simplified
-// the T Class type is options but provides a better type safety for return type
+//Axios requests simplified
+// the T Class type is optional but provides a better type safety for return type.
 const requests = {
     get: <T>(url: string, params?: URLSearchParams) => axios.get<T>(url, { params }).then(responseBody),
     post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
