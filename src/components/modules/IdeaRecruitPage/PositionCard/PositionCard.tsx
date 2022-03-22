1e76852bf20eba0b3259ae95c495e768b60323eb
Mohammedi Abdenasser <75934365+mdinacer@users.noreply.github.com>
2022-03-23 00:51:50 +0100

    Added Expendable position card

---
+++ src/components/modules/IdeaRecruitPage/PositionCard/PositionCard.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import CardTag from "../../../common/CardTag/CardTag";
+import RoundedButton from "../../../common/RoundedButton";
+import {
+  Card,
+  CardSection,
+  Description,
+  DetailsSection,
+  ExpandButton,
+  PositionList,
+  PositionListItem,
+  PositionListTitle,
+  PositionsListContainer,
+  TagsContainer,
+  Title,
+  TitleSection,
+} from "./StyledPositionCard";
+
+interface Props {
+  position: any;
+}
+
+export default function PositionCard({ position }: Props) {
+  const [isExpanded, setIsExpanded] = useState(true);
+  return (
+    <Card>
+      <CardSection>
+        <TitleSection>
+          <Title>{position.title}</Title>
+        </TitleSection>
+        <DetailsSection>
+          <TagsContainer>
+            <CardTag dark>{position.level}</CardTag>
+            {position.stack.map((item: string, index: number) => (
+              <CardTag key={index}>{item}</CardTag>
+            ))}
+          </TagsContainer>
+          <Description>{position.description}</Description>
+        </DetailsSection>
+      </CardSection>
+
+      {isExpanded && (
+        <CardSection>
+          <TitleSection>
+            <RoundedButton
+              fontSize={1.5}
+              paddingX={1.25}
+              paddingY={0.625}
+              bgColor="#454D58"
+              textColor="#FFF"
+            >
+              Apply
+            </RoundedButton>
+          </TitleSection>
+          <DetailsSection>
+            <PositionsListContainer>
+              <PositionListTitle>Expectations</PositionListTitle>
+              <PositionList>
+                {position.expectations.map(
+                  (expectation: string, index: number) => (
+                    <PositionListItem key={index}>
+                      {expectation}
+                    </PositionListItem>
+                  )
+                )}
+              </PositionList>
+            </PositionsListContainer>
+          </DetailsSection>
+        </CardSection>
+      )}
+
+      <ExpandButton
+        isExpanded={isExpanded}
+        type="button"
+        fontSize={0.85}
+        onClick={() => setIsExpanded((prev) => !prev)}
+      >
+        {isExpanded ? "Show Less" : "Show More"}
+      </ExpandButton>
+    </Card>
+  );
+}
