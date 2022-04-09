42a254f1a836f948bcf2c7a55b2c9054efb565df
mfvil <rarefyev@gmail.com>
2022-04-09 10:28:55 -0500

    Roman/header-component: Re-theming the header component based on the new design.

--- src/components/modules/DetailedPage/ProductHeader/StyledProductHeader.js
+++ src/components/modules/DetailedPage/ProductHeader/StyledProductHeader.js
@@ -10,17 +10,47 @@ export const Tags = styled.div`
   border-radius: 20px;
 `;
 
-export const HeaderBlock = styled.div`
-  background-color: #30363e;
-  min-height: 7.5vh;
-  font-family: "Roboto", sans-serif;
-  padding: 1rem;
-  border-radius: 25px 25px 0 0;
+export const ColumnTitle = styled.div`
+  margin-top: 1rem;
+  font-family: Abel;
+  color: #f1f4f5;
+  font-size: 3rem;
+  font-weight: 400;
+  line-height: 1.125rem;
+  padding: 0.375rem;
 `;
 
-export const Column = styled.div`
+export const HeaderBlock = styled.div`
+  min-height: 7.5vh;
+  font-family: Noto Sans;
+`;
+
+export const Column1 = styled.div`
+  background-color: #30363e;
   display: flex;
+  width: 50%;
+  lign-items: flex-start;
   flex-direction: column;
+  border-radius: 25px 25px 0px 0px;
+  padding: 1rem;
+`;
+export const Column2 = styled.div`
+  background-color: #4f5154;
+  display: flex;
+  width: 30%;
+  align-items: flex-start;
+  flex-direction: column;
+  border-radius: 25px 25px 0px 0px;
+  padding: 1rem;
+`;
+export const Column3 = styled.div`
+  background-color: #59687b;
+  display: flex;
+  width: 20%;
+  align-items: flex-end;
+  flex-direction: column;
+  border-radius: 25px 25px 0px 0px;
+  padding: 1rem;
 `;
 
 export const Row = styled.div`
@@ -34,15 +64,6 @@ export const Row = styled.div`
   }
 `;
 
-export const HeaderTitle = styled.div`
-  color: #f1f4f5;
-  font-size: 3rem;
-  font-weight: 700;
-  font-style: bold;
-  margin-top: 1rem;
-  margin-left: 1rem;
-  line-height: 18px;
-`;
 export const HeaderSubtitle = styled.div`
   color: #d3d4d6;
   font-size: 1.5rem;
@@ -52,6 +73,18 @@ export const HeaderSubtitle = styled.div`
   margin-left: 1rem;
   line-height: 40px;
 `;
+
+export const Description = styled.div`
+  color: #d3d4d6;
+  font-family: Noto Sans;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-style: italic;
+  margin-top: 1rem;
+  margin-left: 1rem;
+  line-height: 2.5rem;
+`;
+
 export const Username = styled.div`
   color: #c4c4c4;
   font-size: 1.5rem;
