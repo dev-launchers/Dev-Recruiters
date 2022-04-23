6ddb19a13b2321bc66e8f7526ee52dad0731f8eb
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-04-23 10:59:48 +0300

    used the theme for all the colors

--- src/components/modules/DetailedPage/ProductHeader/StyledProductHeader.ts
+++ src/components/modules/DetailedPage/ProductHeader/StyledProductHeader.ts
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const Tags = styled.div`
-  color: #c4c4c4;
+  color: ${({ theme }) => theme.colors.SilverSand};
   font-size: 1.125rem;
   font-weight: 400;
   line-height: 21px;
   padding: 6px;
-  border: 1px solid rgba(211, 212, 214, 0.9);
+  border: 1px solid ${({ theme }) => theme.colors.LightGrayT90};
   border-radius: 20px;
 `;
 
 export const ColumnTitle = styled.div`
   margin-top: 1rem;
   font-family: ${({ theme }) => theme.fonts.headline};
-  color: #f1f4f5;
+  color: ${({ theme }) => theme.colors.AntiFlashWhite};
   font-size: 3rem;
   font-weight: 400;
   line-height: 1.125rem;
@@ -25,8 +25,9 @@ export const HeaderBlock = styled.div`
   font-family: ${({ theme }) => theme.fonts.normal};
 `;
 
-export const Column = styled.div`
-  background-color: ${({ bg }) => (bg ? bg : "#30363E")};
+export const Column = styled.div<{ bgColor?: string; w?: string; fa?: string }>`
+  background-color: ${({ bgColor, theme }) =>
+    bgColor ? bgColor : theme.colors.Crayola};
   width: ${({ w }) => (w ? w : "50%")};
   align-items: ${({ fa }) => (fa ? fa : "flex-start")};
   display: flex;
@@ -47,7 +48,7 @@ export const Row = styled.div`
 `;
 
 export const Type = styled.div`
-  color: #d3d4d6;
+  color: ${({ theme }) => theme.colors.LightGray};
   font-size: 1.5rem;
   font-weight: 400;
   font-style: italic;
@@ -57,7 +58,7 @@ export const Type = styled.div`
 `;
 
 export const Vision = styled.div`
-  color: #d3d4d6;
+  color: ${({ theme }) => theme.colors.LightGray};
   font-size: 1.5rem;
   font-weight: 500;
   margin-left: 1rem;
@@ -65,7 +66,7 @@ export const Vision = styled.div`
 `;
 
 export const Username = styled.div`
-  color: #c4c4c4;
+  color: ${({ theme }) => theme.colors.SilverSand};
   font-size: 1.5rem;
   font-weight: 400;
   margin-right: 1rem;
@@ -76,15 +77,15 @@ export const UsernameAvatar = styled.img`
   height: 2.1875rem;
   margin-right: 1rem;
 `;
-export const Date = styled.div`
-  color: #c4c4c4;
+export const CreationDate = styled.div`
+  color: ${({ theme }) => theme.colors.SilverSand};
   font-size: 1.5rem;
   font-weight: 400;
   margin-right: 1rem;
   line-height: 2.1875rem;
 `;
 export const Commitment = styled.div`
-  color: #c4c4c4;
+  color: ${({ theme }) => theme.colors.SilverSand};
   font-size: 1.5rem;
   font-weight: 400;
   margin-right: 1rem;
