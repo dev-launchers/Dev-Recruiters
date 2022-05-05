e292cb2fcdbda49e88b3b35741d7ab77ca57163e
Mohammedi Abdenasser <75934365+mdinacer@users.noreply.github.com>
2022-05-06 00:28:41 +0100

    Updated Opportunities cards to new desgin Added Responsive styles

--- src/components/modules/DetailedPage/PositionCard/StyledPositionCard.ts
+++ src/components/modules/DetailedPage/PositionCard/StyledPositionCard.ts
@@ -1,108 +1,279 @@
 import styled from "styled-components";
-import { RoundedButtonProps } from "@components/common/RoundedButton/StyledRoundedButton";
 
-export const Card = styled.div`
-  position: relative;
-  width: auto;
-  height: auto;
-  background-color: #c3c0c0;
-  border-radius: 2.5rem;
-  padding: 1.5rem 2.8rem;
-  transition: height 1s ease-in-out;
+export const Container = styled.div`
+position: relative;
+display: grid;
+grid-template-columns: repeat(1, minmax(0, 1fr));
+background: ${(props) => props.theme.colors.White};
+border-radius: 1.8rem;
+overflow: hidden;
+
+@media (min-width: 1024px) {
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+}
 `;
 
-export const CardSection = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  column-gap: 2rem;
-  height: auto;
+type sectionTheme = 'Dark' | 'Light';
+
+export const Section = styled.div<{
+  color: sectionTheme;
+  Mobile: boolean;
+  Expanded?: boolean;
+}>`
+border-radius: 1.8rem;
+padding: 1rem;
+background-color: ${(props) =>
+    props.color === 'Dark'
+      ? props.theme.colors.Crayola
+      : props.theme.colors.White};
+
+color: ${(props) =>
+    props.color === 'Light'
+      ? props.theme.colors.OuterSpace
+      : props.theme.colors.White};
+
+@media (max-width: 640px) {
+  display: ${({ Mobile, Expanded }) =>
+    Mobile ? (Expanded ? 'block' : 'none') : 'block'};
+}
+`;
+
+export const OpportunityInfoContainer = styled.div`
+width: 100%;
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+margin: auto auto;
+
+@media (max-width: 640px) {
+  flex-direction: column;
+}
 `;
 
 export const TitleSection = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex: 0 0 12%;
-`;
+padding: 0 1rem;
+width: 10rem;
 
-export const DetailsSection = styled.div`
-  height: auto;
-
-  width: auto;
-`;
-
-export const Title = styled.p`
-  width: "15%";
-  height: auto;
-  padding: 0;
-  margin: 0;
-  font-family: ${({ theme }) => theme.fonts.headline}, sans-serif;
+& h2 {
+  font-family: ${(props) => props.theme.fonts.normal};
+  font-size: 1.44rem;
   font-style: normal;
-  font-weight: 500;
-  font-size: 2.5rem;
-  line-height: 2.4375rem;
-  max-width: 13rem;
-`;
-
-export const Description = styled.p`
-  font-family: ${({ theme }) => theme.fonts.normal}, sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 1.12rem;
-  line-height: 1.3125rem;
-  color: #454d58;
-`;
-
-export const TagsContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-items: flex-start;
-  column-gap: 0.9375rem;
-  width: auto;
-`;
-
-export const PositionsListContainer = styled.div`
-  height: auto;
-  display: flex;
-  flex-direction: column;
-`;
-export const PositionListTitle = styled.p`
-  font-family: ${({ theme }) => theme.fonts.headline}, sans-serif;
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin: 0;
-  padding: 0;
-`;
-export const PositionList = styled.ul`
-  padding: 0;
-  margin: 0.75rem;
-`;
-export const PositionListItem = styled.li`
-  font-family: ${({ theme }) => theme.fonts.normal}, sans-serif;
-  font-size: 1rem;
-  font-weight: 500;
-  margin: 0;
-  padding: 0;
-`;
-
-interface ExpandButtonProps extends RoundedButtonProps {
-  isExpanded: boolean;
+  font-weight: 700;
+  line-height: 2rem;
 }
 
-export const ExpandButton = styled.button<ExpandButtonProps>`
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  margin: ${(props) => (props.isExpanded ? "1.87rem 2.5rem" : ".62rem 2.5rem")};
-  font-family: ${({ theme }) => theme.fonts.normal}, sans-serif;
-  font-size: ${(props) =>
-    props.fontSize ? `${props.fontSize}rem` : "1.12rem"};
-  background: ${(props) => props.bgColor ?? "rgba(84, 87, 91, 0.2)"};
-  color: ${(props) => props.textColor ?? "inherit"};
-  border-radius: ${(props) => props.borderRadius ?? "1.25rem"};
-  border: none;
-  padding: 0.31rem 0.62rem;
+@media (max-width: 640px) {
+  width: 100%;
+} ;
+`;
+
+export const DescriptionSection = styled.div<{ Expanded: boolean; Mobile: boolean }>`
+display: ${(props) => (props.Mobile ? 'none' : 'block')};
+
+& h3 {
+  font-family: ${(props) => props.theme.fonts.normal};
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 1.37rem;
+}
+
+& p {
+  height: ${(props) => (props.Expanded ? 'auto' : 'auto')};
+  font-family: ${(props) => props.theme.fonts.normal};
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.8138rem;
+  line-height: 1.125rem;
+  max-width: 28rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: pre-wrap;
+}
+
+@media (max-width: 640px) {
+  display: ${(props) => (props.Mobile ? 'block' : 'none')};
+  & p {
+    padding: 0 1rem;
+    height: ${(props) => (props.Expanded ? 'auto' : 'auto')};
+    font-family: ${(props) => props.theme.fonts.normal};
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.125rem;
+  }
+}
+`;
+
+export const OpportunityDetailsContainer = styled.div`
+width: 100%;
+display: flex;
+flex-direction: row;
+margin: auto auto;
+font-family: ${(props) => props.theme.fonts.normal};
+
+& h4 {
+  font-style: normal;
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: 22px;
+}
+`;
+
+export const TagsSection = styled.div`
+padding: 0 1rem;
+max-width: 50%;
+width: 50%;
+@media (max-width: 640px) {
+  display: none;
+} ;
+`;
+export const TagsList = styled.ul`
+list-style: none;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+padding: 0;
+margin: 0;
+`;
+export const TagsListItem = styled.li<{ color: sectionTheme }>`
+padding: 0.2rem 1rem;
+margin: 0 3px;
+background-color: ${(props) =>
+    props.color === 'Dark'
+      ? props.theme.colors.OuterSpace
+      : props.theme.colors.White};
+
+color: ${(props) =>
+    props.color === 'Light'
+      ? props.theme.colors.OuterSpace
+      : props.theme.colors.Argent};
+border: ${({ theme }) => `0.723357px solid ${theme.colors.OuterSpace}`};
+filter: ${({ theme }) =>
+    `drop-shadow(0px 0.18rem 0.18rem ${theme.colors.BlackT38})`};
+
+border-radius: 1.8rem;
+
+font-family: ${(props) => props.theme.fonts.normal};
+font-style: normal;
+font-weight: 400;
+font-size: 13.0204px;
+line-height: 18px;
+text-transform: capitalize;
+`;
+
+export const CommitmentSection = styled.div`
+margin-right: auto;
+min-width: 50%;
+padding: 0 1rem;
+& p {
+  font-family: ${(props) => props.theme.fonts.normal};
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.81rem;
+  line-height: 18px;
+  margin-left: 1rem;
+}
+@media (max-width: 640px) {
+  display: none;
+}
+`;
+
+export const ExpectationsSection = styled.div<{ Expanded: boolean }>`
+display: ${(props) => (props.Expanded ? 'block' : 'none')};
+margin-right: auto;
+width: auto;
+
+padding: 0 1rem;
+& h4 {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 22px;
+}
+
+@media (max-width: 640px) {
+  display: block;
+  margin: 0 0;
+}
+`;
+
+export const ExpectationsList = styled.ul`
+list-style: disc;
+padding: 0;
+margin: 0;
+margin-left: 1.25rem;
+`;
+export const ExpectationsListItem = styled.li`
+font-family: ${(props) => props.theme.fonts.normal};
+font-style: normal;
+font-weight: 400;
+font-size: 13.0204px;
+line-height: 9px;
+`;
+
+export const ButtonsSection = styled.div<{ Mobile: boolean }>`
+display: ${(props) => (props.Mobile ? 'none' : 'flex')};
+width: 100%;
+flex-direction: row;
+justify-content: flex-end;
+margin-left: auto;
+margin-top: 2rem;
+
+@media (max-width: 640px) {
+  display: ${(props) => (props.Mobile ? 'flex' : 'none')};
+  margin: auto auto;
+  justify-content: space-evenly;
+}
+`;
+
+export const Button = styled.button<{ color?: string }>`
+font-family: ${(props) => props.theme.fonts.normal};
+background-color: ${(props) =>
+    props.color
+      ? props.theme.colors[props.color]
+      : props.theme.colors.OuterSpace};
+color: ${(props) => props.theme.colors.White};
+border-radius: 1.8rem;
+padding: 0.75rem 2rem;
+margin: 0 3px;
+border: none;
+font-style: normal;
+font-weight: 400;
+font-size: 0.9rem;
+line-height: 11px;
+text-align: center;
+`;
+
+export const PositionDetailsMobile = styled.div`
+padding: 0 12px;
+& p {
+  font-family: ${(props) => props.theme.fonts.normal};
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.125rem;
+  margin-top: 0;
+}
+@media (min-width: 641px) {
+  display: none;
+}
+`;
+
+export const LikeButton = styled.button`
+position: absolute;
+top: 0;
+right: 0;
+border-radius: 50%;
+border: none;
+margin: 1rem;
+background: none;
+`;
+
+export const Icon = styled.svg<{ Active: boolean }>`
+color: ${(props) =>
+    props.Active ? props.theme.colors.White : props.theme.colors.Crayola};
+stroke: ${(props) => props.theme.colors.White};
+height: 2rem;
+width: 2rem;
 `;
--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,13 +1,14 @@
-import { GetStaticProps, GetStaticPaths } from "next";
-import Head from "next/head";
-import PositionCard from "@components/modules/DetailedPage/PositionCard";
-import { Project } from "../models/project";
+import { GetStaticProps, GetStaticPaths } from 'next';
+import Head from 'next/head';
+import PositionCard from '@components/modules/DetailedPage/PositionCard';
+import { Project } from '../models/project';
+import styled from 'styled-components';
 
 export const getProjectsSlugs = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_STRAPI_URL}/projects?_publicationState=live`
   );
-  
+
   const result: Project[] = await res.json();
   const projects = result?.filter((p) => p.opportunities?.length > 0);
 
@@ -20,18 +21,17 @@ export const getProjectsSlugs = async () => {
   return projectsSlugs;
 };
 
-
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = await getProjectsSlugs();
   return {
     paths,
-    fallback: "blocking",
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/${params.slug}`
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/projects/${params.slug}`
   );
   const project: Project = await res.json();
 
@@ -47,21 +47,25 @@ interface Props {
   project: Project;
 }
 
-export default function DetailedPage({ project }: Props) {
-  const samplePosition = {
-    id: "1",
-    title: "Web Developer",
-    level: "Beginner Level",
-    stack: ["React", "html", "css"],
-    expectations: ["Self Sufficient learner", "Hands on learning experience", "Weekly meetings"],
-    description: ` We are looking for a web developer who has some knowledge of
-    React.js, HTML, and CSS. Experience using API's is appreciated but
-    not required. All experience levels welcome, provided you are a
-    willing and self-sufficient learner and a good communicator. You
-    will:`,
-  };
+// export default function DetailedPage({ project }: Props) {
 
-export default function DetailedPage() {
+// }
+
+const samplePosition = {
+  id: '1',
+  title: 'Web Developer',
+  level: 'Beginner',
+  skills: ['React', 'html', 'css'],
+  commitment: '5 hrs per week',
+  expectations: [
+    'Self Sufficient learner',
+    'Hands on learning experience',
+    'Weekly meetings',
+  ],
+  description: `We are looking for a web developer who has some knowledge of React.js, HTML, and CSS. Experience using API's is appreciated but not required. All experience levels welcome, provided you are a willing and self-sufficient learner and a good communicator. You will: -work on a team of 6+ people -work alongside and coordinate with Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+};
+
+export default function DetailedPage({ project }: Props) {
   return (
     <>
       <Head>
@@ -72,52 +76,80 @@ export default function DetailedPage() {
           content='Create, discover, and join open-source software projects! We help members to contribute meaningfully and gain industry-ready experience along the way. Build epic products, tools, and games used by real people while learning valuable skills and meeting awesome people!'
         ></meta>
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://devlaunchers.org/projects" />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://devlaunchers.org/projects' />
         <meta
-          property="og:image"
-          content="/images/DevlaunchersGitHubThumb.png"
+          property='og:image'
+          content='/images/DevlaunchersGitHubThumb.png'
         />
-        <meta property="og:title" content="Our Projects" />
+        <meta property='og:title' content='Our Projects' />
         <meta
           property='og:description'
           content='Create, discover, and join open-source software projects! We help members to contribute meaningfully and gain industry-ready experience along the way. Build epic products, tools, and games used by real people while learning valuable skills and meeting awesome people!'
         ></meta>
 
-        <meta property="twitter:card" content="summary_large_image" />
+        <meta property='twitter:card' content='summary_large_image' />
         <meta
-          property="twitter:url"
-          content="https://devlaunchers.org/projects"
+          property='twitter:url'
+          content='https://devlaunchers.org/projects'
         />
-        <meta property="twitter:title" content="Our Projects" />
+        <meta property='twitter:title' content='Our Projects' />
         <meta
-          property="twitter:description"
-          content="Create, discover, and join open-source software projects! We help members to contribute meaningfully and gain industry-ready experience along the way. Build epic products, tools, and games used by real people while learning valuable skills and meeting awesome people!"
+          property='twitter:description'
+          content='Create, discover, and join open-source software projects! We help members to contribute meaningfully and gain industry-ready experience along the way. Build epic products, tools, and games used by real people while learning valuable skills and meeting awesome people!'
         />
         <meta
-          property="twitter:image"
-          content="/images/DevlaunchersGitHubThumb.png"
+          property='twitter:image'
+          content='/images/DevlaunchersGitHubThumb.png'
         />
-        <meta content="#ff7f0e" data-react-helmet="true" name="theme-color" />
+        <meta content='#ff7f0e' data-react-helmet='true' name='theme-color' />
       </Head>
       <h1>hello from project details page</h1>
       {/* testing postion Card */}
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          rowGap: "1.5rem",
-        }}
-      >
-        {project.opportunities.length &&
+      <Container>
+        <p>Available Positions</p>
+        {/* {project.opportunities?.length > 0 &&
           project.opportunities.map((position) => (
-            <PositionCard
-              key={position.id}
-              position={samplePosition}
-            />
+            <PositionCard key={position.id} position={samplePosition} />
+          ))} */}
+        <PositionsList>
+          {[1, 2, 4, 5].map((position) => (
+            <PositionCard key={position} position={samplePosition} />
           ))}
-      </div>
+        </PositionsList>
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.Argent};
+  border-radius: 30px;
+  padding-bottom: 2rem;
+
+  & > p {
+    padding: 0.625rem 1.25rem;
+    margin-top: 0;
+    background-color: ${({ theme }) => theme.colors.CoolGrey};
+    border-radius: 30px;
+    font-family: ${({ theme }) => theme.fonts.normal};
+    font-style: normal;
+    font-weight: 600;
+    font-size: 19.5px;
+    line-height: 27px;
+    text-transform: capitalize;
+  }
+`;
+
+const PositionsList = styled.div`
+  padding: 0 24px;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.5rem;
+
+  @media (max-width: 640px) {
+    padding: 0 5px;
+  }
+`;
