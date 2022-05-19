a227917434097c939b102cbcf255b9bf2b674983
Mohammedi Abdenasser <75934365+mdinacer@users.noreply.github.com>
2022-05-19 23:57:11 +0100

    Changed BoxContainer height to auto

--- src/components/common/BoxContainer/StyledBoxContainer.ts
+++ src/components/common/BoxContainer/StyledBoxContainer.ts
@@ -1,46 +1,46 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div<{
   bgColor?: string;
   marginTop?: number;
   marginBottom?: number;
 }>`
-overflow: hidden;
-width: 100;
-height: 100%;
-    background: ${({ theme, bgColor }) =>
+  overflow: hidden;
+  width: 100;
+  height: auto;
+  background: ${({ theme, bgColor }) =>
     bgColor ? theme.colors[bgColor] : 'transparent'};
-  `;
+`;
 
 export const Content = styled.div<{
   paddingHorizontal?: number;
   paddingVertical?: number;
 }>`
-    box-sizing: border-box;
-    width: 100%;
-    margin: auto auto;
-    padding-top: ${({ paddingVertical }) =>
+  box-sizing: border-box;
+  width: 100%;
+  margin: auto auto;
+  padding-top: ${({ paddingVertical }) =>
     paddingVertical ? `${paddingVertical}px` : 0};
-    padding-bottom: ${({ paddingVertical }) =>
+  padding-bottom: ${({ paddingVertical }) =>
     paddingVertical ? `${paddingVertical}px` : 0};
-    padding-left: ${({ paddingHorizontal }) =>
+  padding-left: ${({ paddingHorizontal }) =>
     paddingHorizontal ? `${paddingHorizontal}px` : 0};
-    padding-right: ${({ paddingHorizontal }) =>
+  padding-right: ${({ paddingHorizontal }) =>
     paddingHorizontal ? `${paddingHorizontal}px` : 0};
-  
-    @media (min-width: 640px) {
-      max-width: 640px;
-    }
-    @media (min-width: 768px) {
-      max-width: 768px;
-    }
-    @media (min-width: 1024px) {
-      max-width: 1024px;
-    }
-    @media (min-width: 1280px) {
-      max-width: 1280px;
-    }
-    @media (min-width: 1536px) {
-      max-width: 1536px;
-    }
-  `;
+
+  @media (min-width: 640px) {
+    max-width: 640px;
+  }
+  @media (min-width: 768px) {
+    max-width: 768px;
+  }
+  @media (min-width: 1024px) {
+    max-width: 1024px;
+  }
+  @media (min-width: 1280px) {
+    max-width: 1280px;
+  }
+  @media (min-width: 1536px) {
+    max-width: 1536px;
+  }
+`;
