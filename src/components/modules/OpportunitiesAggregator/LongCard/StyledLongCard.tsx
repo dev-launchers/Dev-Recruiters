257575f8fd1a7a422c358a759aaf81d496e35664
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-06-21 18:20:40 +0300

    fix(Enjoy2Live): left margin on bullet points description

--- src/components/modules/OpportunitiesAggregator/LongCard/StyledLongCard.tsx
+++ src/components/modules/OpportunitiesAggregator/LongCard/StyledLongCard.tsx
@@ -8,6 +8,10 @@ export const Container = styled.div`
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   display: flex;
   flex-direction: column;
+  
+  ul {
+    margin-left: 2rem;
+  }
   // @media (orientation: portrait) {
   //   margin-bottom: 5%;
   // }
